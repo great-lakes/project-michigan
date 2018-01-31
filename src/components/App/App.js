dc197165c3a0c31284bbb89161df1704c84b9832
Kevin Leung <keleung@microsoft.com>
2018-01-31 11:31:11 -0600

    removing .js on import and restoring App.js

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router'
+import Placeholder from '../Placeholder'
+import TabMenu from '../TabMenu'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <TabMenu />
+        <Switch>
+          <Route path='/questions' component={Placeholder} />
+          <Route path='/projects' component={Placeholder} />
+          <Route path='/azureCodes' component={Placeholder} />
+          <Route path='/mentors' component={Placeholder} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default App
