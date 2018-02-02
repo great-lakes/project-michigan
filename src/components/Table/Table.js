29103e37a020160ac1c0eea95d0aad2e9943d330
gcrev93 <gcrev93@gmail.com>
2018-02-02 15:03:03 -0600

    table component ready to merge

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './Table.css'
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
 
 const data = [{ // fake data for example. will delete
   resolved: false,
@@ -12,72 +14,72 @@ const data = [{ // fake data for example. will delete
   student: 'Billy Bob',
   project: 'IoT Cloud',
   contact: 'bb@live.com'
-}, {
-  resolved: false,
-  mentor: 'Kevin L',
-  mentorStatus: true,
-  skill: 'CaaP',
-  question: 'where are the bot framework docs?',
-  student: 'Billy Joe Bob',
-  project: 'Da Big Bot',
-  contact: 'bb@outlook.com'
-}, {
-  resolved: true,
-  mentor: 'Gabby C',
-  mentorStatus: true,
-  skill: 'Azure search',
-  question: 'how do i import docs from Cosmos DB?',
-  student: 'Bob Guy',
-  project: 'Search IT!',
-  contact: 'bg@live.com'
 }]
 
-function chooseColor (mentorStatus, resolvedStatus) {
-  if (mentorStatus && resolvedStatus) {
-    return { color: '#57d500'}
-  } else if (mentorStatus && !resolvedStatus) {
-    return { color: '#fffe00' }
-  } else {
-    return { color: '#ff0000' }
-  }
+const UNASSIGNED = 'unassigned'
+const ASSIGNED = 'assigned'
+const RESOLVED = 'resolved'
+
+function determineStatus (mentorStatus, resolvedStatus) {
+  if (mentorStatus && resolvedStatus) return RESOLVED
+  if (mentorStatus && !resolvedStatus) return ASSIGNED
+  return UNASSIGNED
+}
+
+function chooseStatusColor (status) {
+  if (status === RESOLVED) return { color: '#57d500' }
+  if (status === ASSIGNED) return { color: '#fffe00' }
+  return { color: '#ff0000' }
 }
 
 // Next fix mentor field to update status field
 class Table extends Component {
   handleClick (e, mentorStatus, resolvedStatus) {
     if (mentorStatus && !resolvedStatus) {
-      resolvedStatus =
+      // TODO: dispatch to change resolveStatus
       console.log('change from green to yellow')
-      // dispactch to change resolveStatus
     }
   }
-  renderEditable (cellInfo) {
-    return (
-      <div
-        contentEditable
-        onBlur={e => {
-          // run a dispatch
-          // update mentorStatus to true
-        }}
-      />
-    )
-  }
+  // TODO: replace with dropdown of mentors
+  // renderEditable (cellInfo) {
+  //   return (
+  //     <div
+  //       contentEditable
+  //       onBlur={e => {
+  //         // run a dispatch
+  //         // update mentorStatus to true
+  //       }}
+  //     />
+  //   )
+  // }
   render () {
     const columns = [{ // change to button. on click: if red do nothing. if yellow turn green
-      Header: '',
-      accessor: 'mentorStatus',
-      Cell: d => (
-        <button onClick={(e) => this.handleClick(e, d.value, d.original.resolved)}>
-          <span style={chooseColor(d.value, d.original.resolved)}>
+      Header: 'Status',
+      id: 'question-status',
+      accessor: d => d.mentorStatus,
+      Cell: d => {
+        const status = determineStatus(d.value, d.original.resolved) // TODO: disable button on red & green
+        return (<button onClick={(e) => this.handleClick(e, d.value, d.original.resolved)} title={status}>
+          <span style={chooseStatusColor(status)}>
             &#x25cf;
           </span>
-        </button>
-      )
+        </button>)
+      }
     }, {
       Header: 'Mentor',
-      id: 'name',
+      id: 'mentor',
       accessor: d => d.mentor,
-      Cell: this.renderEditable
+      Cell: d => {
+        return (<Select
+          name='mentor-name'
+          value='one'
+          onChange={this.handleChange}
+          options={[
+          { value: 'one', label: 'One' },
+          { value: 'two', label: 'Two' }
+          ]}
+      />)
+      } // TODO: change based on dropdown function
     }, {
       Header: 'Skill',
       id: 'skill',
@@ -101,6 +103,14 @@ class Table extends Component {
     }]
     return (
       <ReactTable
+        getTdProps={(state, rowInfo, column) => {
+          return {
+            style: {
+              whiteSpace: 'normal',
+              overflow: 'visible'
+            }
+          }
+        }}
         data={data}
         columns={columns}
         />
